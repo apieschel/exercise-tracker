3d04f16f58d93317ce537bd07b831b6572297d78
Glitch (apieschel-exercise-tracker) <none>
2019-02-04 20:56:57 +0000

    updates_2_4_19

--- routes.js
+++ routes.js
@@ -32,67 +32,64 @@ module.exports = function (app, db) {
 							fromDate = new Date(req.params.from);
 							toDate = new Date(req.params.to);
 							limit = parseInt(req.params.limit);
-							console.log(limit);
 							
-							console.log("from: " + fromDate);
-							console.log("to: " + toDate);
-							
+              // make sure the dates are valid and the limit is a number
 							if(fromDate == "Invalid Date" || toDate == "Invalid Date" || isNaN(limit)) {
-								res.json("Sorry, you didn't enter a valid date. Or your limit wasn't a valid integer, or whole number.");		
+								res.json("Sorry, either you didn't enter a valid date, or your limit wasn't a valid integer.");		
 							} else {
+                // loop through the exercise array and put the ones that satisfy the parameters in a new array
 								for(let i = 0; i < data.exercises.length; i++) {
 									exerciseDate = new Date(data.exercises[i].date);
 									duration = data.exercises[i].duration;
-									console.log("exercise date: " + exerciseDate);
+                  
 									if(exerciseDate >= fromDate && exerciseDate <= toDate && duration <= limit) {
 										workouts.push(data.exercises[i]);
 									}
 								}
-								console.log("workouts: " + workouts);
 								res.json(workouts);	
 							}
 						}
-					
+					  
+            // if you only have the date parameters
 						else if(req.params.from && req.params.to) {
 							fromDate = new Date(req.params.from);
 							toDate = new Date(req.params.to);
-							console.log("from: " + fromDate);
-							console.log("to: " + toDate);
 							
+              // make sure the dates are valid
 							if(fromDate == "Invalid Date" || toDate == "Invalid Date") {
 								res.json("Sorry, you didn't enter a valid date.");		
 							} else {
 								for(let i = 0; i < data.exercises.length; i++) {
 									exerciseDate = new Date(data.exercises[i].date);
-									console.log("exercise date: " + exerciseDate);
+                  
 									if(exerciseDate >= fromDate && exerciseDate <= toDate) {
 										workouts.push(data.exercises[i]);
 									}
 								}
-								console.log("workouts: " + workouts);
 								res.json(workouts);
 							}
 						}
-					
+					  
+            // if there's only a "from" date, but no "to" date, then just return all exercises after the "from" date
 						else if(req.params.from) {
 							fromDate = new Date(req.params.from);
-							console.log("from: " + fromDate);
 							
 							if(fromDate == "Invalid Date") {
 								res.json("Sorry, you didn't enter a valid date.");		
 							} else {
 								for(let i = 0; i < data.exercises.length; i++) {
 									exerciseDate = new Date(data.exercises[i].date);
-									console.log("exercise date: " + exerciseDate);
+                  
 									if(exerciseDate >= fromDate) {
 										workouts.push(data.exercises[i]);
 									}
 								}
-								console.log("workouts: " + workouts);
+                
 								res.json(workouts);
 							}
 						} 
-					
+					  
+            // otherwise, just return the full exercise array
 						else {
 							res.json(data.exercises);
 						}
